e8c0d2074cbae80e90d42e08b50bdd47076be2fc
Steven <stevencraig321@gmail.com>
2025-03-02 19:50:31 +0000

    feat: updated the hero section

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,41 +1,25 @@
-import Link from 'next/link';
-
 import { Background } from '../background/Background';
-import { Button } from '../button/Button';
 import { HeroOneButton } from '../hero/HeroOneButton';
 import { Section } from '../layout/Section';
-import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Logo } from './Logo';
 
 const Hero = () => (
   <Background color="bg-gray-100">
     <Section yPadding="py-6">
-      <NavbarTwoColumns logo={<Logo xl />}>
-        <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            GitHub
-          </Link>
-        </li>
-        <li>
-          <Link href="/">Sign in</Link>
-        </li>
-      </NavbarTwoColumns>
+      <Logo xl />
     </Section>
 
     <Section yPadding="pt-20 pb-32">
       <HeroOneButton
         title={
           <>
-            {'The modern landing page for\n'}
-            <span className="text-primary-500">React developers</span>
+            {'Professional Pet Care Services in Aviemore\n'}
+            <span className="text-primary-500">
+              Group Walks • Pet Sitting • Solo Walks
+            </span>
           </>
         }
-        description="The easiest way to build a React landing page in seconds."
-        button={
-          <Link href="https://creativedesignsguru.com/category/nextjs/">
-            <Button xl>Download Your Free Theme</Button>
-          </Link>
-        }
+        description="Providing trusted, reliable pet care services in the heart of the Cairngorms. We offer professional dog walking, pet sitting, and customized care for your beloved pets."
       />
     </Section>
   </Background>
--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,7 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'Aviemore Pet Services',
+  title: 'Aviemore Pet Services',
+  description:
+    'Aviemore Pet Services offers dog walking, pet sitting, and pet taxi services in Aviemore and the surrounding area.',
   locale: 'en',
 };
